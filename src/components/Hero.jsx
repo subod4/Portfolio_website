a61e02796441031b0023a804005b9504d9910dc7
Subodh Badal <subodhbadal111@gmail.com>
2025-05-30 22:19:59 +0545

    deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,8 +20,8 @@ const Hero = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2 }}
         >
-          <span className="text-cyber-blue">JOHN</span>
-          <span className="text-cyber-pink"> DOE</span>
+          <span className="text-cyber-blue">SUBODH</span>
+          <span className="text-cyber-pink"> BADAL</span>
         </motion.h1>
         
         <motion.h2 
@@ -30,7 +30,7 @@ const Hero = () => {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.4 }}
         >
-          Aspiring Software Engineer
+          Aspiring Computer Engineer
         </motion.h2>
         
         <motion.div
@@ -41,7 +41,7 @@ const Hero = () => {
         >
           <div className="w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden neon-border">
             <img 
-              src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+              src="/image.png"
               alt="Profile"
               className="w-full h-full object-cover"
             />
